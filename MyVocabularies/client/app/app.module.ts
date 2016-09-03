63685231f1d6395f62179c41200136b90edc6fda
liuxun <liuxun@outlook.com>
2016-09-03 23:20:12 +0800

    finish app.module and apply it

--- MyVocabularies/client/app/app.module.ts
+++ MyVocabularies/client/app/app.module.ts
@@ -1,2 +1,30 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+import {AppComponent} from './app.component';
+import {PhraseComponent} from './phrase.component';
+import {SentenceComponent} from './sentence.component';
+import {PhraseService} from './phrase.service';
+import {routing} from './app.routes';
+
+@NgModule({
+    imports: [BrowserModule,
+        FormsModule,
+        HttpModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        PhraseComponent,
+        SentenceComponent
+    ],
+    providers: [
+        PhraseService,
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
--- MyVocabularies/client/app/app.routes.ts
+++ MyVocabularies/client/app/app.routes.ts
@@ -1,12 +1,11 @@
-import { provideRouter, RouterConfig } from '@angular/router';
+import {ModuleWithProviders} from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import {PhraseComponent} from './phrase.component';
 import {SentenceComponent} from './sentence.component';
 
-const routes: RouterConfig = [
+const appRoutes: Routes = [
     { path: 'phrase', component: PhraseComponent },
     { path: 'sentence', component: SentenceComponent },
     { path: '', redirectTo: '/phrase', pathMatch: 'full' }];
 
-export const APP_ROUTER_PROVIDERS = [
-    provideRouter(routes)
-];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
